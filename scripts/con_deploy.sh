28e00e6d7d906cc1c74ef2ad7cbea0d9b8626f76
ChoiYunWoo <ec2-user@spring-web-server>
2021-04-01 23:43:28 +0900

    modify con_.sh

--- scripts/con_deploy.sh
+++ scripts/con_deploy.sh
@@ -4,6 +4,5 @@ REPOSITORY=/app
 WEBREPOSITORY=/usr/local/tomcat/tomcat9/webapps
 
 echo "> docker command start"
-sudo docker exec -it test2 /bin/bash -c mv REPOSITORY/*.war WEBREPOSITORY && /bin/bash -c /app/tomcat.sh 
-
+sudo docker exec -it test2 /bin/bash -c "mv /app/*.war /usr/local/tomcat/tomcat9/webapps/ && /app/tomcat.sh"
 
