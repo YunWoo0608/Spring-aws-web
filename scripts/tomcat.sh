93a383b660eb08092e959fdffcc8faff8b9e0e4f
ChoiYunWoo <ec2-user@spring-web-server>
2021-04-02 10:21:49 +0900

    modify tomcat.sh

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -4,9 +4,9 @@ echo "> enable, restart tomcat.service "
 
 systemctl daemon-reload
 systemctl enable tomcat.service
-systemctl restart tomcat.service
 
 cp /app/*.war /usr/local/tomcat/tomcat9/webapps
 
 perl -p -i -e '$.==155 and print "<Context path='"''"' docBase='"'/usr/local/tomcat/tomcat9/webapps/myapp-1.0.0-BUILD-SNAPSHOT'"' reloadable='"'true'"'/>\n"' /usr/local/tomcat/tomcat9/conf/server.xml
 
+systemctl restart tomcat.service
