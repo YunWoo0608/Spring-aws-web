f4c2fb22a29e40b443503e35a87b3f22fae19b42
ChoiYunWoo <ec2-user@spring-web-server>
2021-04-02 09:55:39 +0900

    modify CD logic

--- scripts/tomcat.sh
+++ scripts/tomcat.sh
@@ -6,3 +6,5 @@ systemctl daemon-reload
 systemctl enable tomcat.service
 systemctl restart tomcat.service
 
+perl -p -i -e '$.==155 and print "<Context path='"''"' docBase='"'/usr/local/tomcat/tomcat9/webapps/myapp-1.0.0-BUILD-SNAPSHOT'"' reloadable='"'true'"'/>\n"' /usr/local/tomcat/tomcat9/conf/server.xml
+
