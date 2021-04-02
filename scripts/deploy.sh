3afd517a6cd9d6e32649376f320e5524a8cb27fa
ChoiYunWoo <ec2-user@spring-web-server>
2021-04-02 10:17:42 +0900

    fix logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,8 @@ echo "> docker command start"
 sudo chmod -R 775 /var/lib/docker/volumes
 sudo docker volume create --name myvolume
 sudo docker run --privileged -d --name test2 -p 80:8080  -v myvolume:/app $DOCKERIMAGES init
-sudo docker exec test2 /bin/bash -c "cp /app/*.war /usr/local/tomcat/tomcat9/webapps/ && /app/tomcat.sh"
+sudo docker exec test2 /bin/bash -c /app/tomcat.sh
+
 echo "> 현재 구동중인 애플리케이션 pid 확인 "
 
 CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.war)
