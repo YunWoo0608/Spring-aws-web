6180b329cadce01a96f585c3de6b8841ae40122f
ChoiYunWoo <ec2-user@spring-web-server>
2021-04-06 12:29:28 +0900

    deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,6 +18,7 @@ echo "> docker command start"
 #sudo chmod -R 775 /var/lib/docker/volumes
 sudo docker volume create --name myvolume
 sudo docker service create --name test2 -p 80:8080 --mount type=bind,source=$CONTAINERREPO,target=/app --replicas 2 $DOCKERIMAGES init
+
 #sudo docker exec test2 /bin/bash -c /app/tomcat.sh
 
 echo "> 현재 구동중인 애플리케이션 pid 확인 "
