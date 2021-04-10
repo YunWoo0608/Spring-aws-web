b9994bb8323e026394f156b7baeafce932fbf220
ChoiYunWoo <ec2-user@spring-web-server>
2021-04-10 20:29:58 +0900

    add promethues and fix files(stack deploy)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,45 +5,46 @@ PROJECT_NAME=Spring-aws-web
 WEBREPOSITORY=/usr/local/tomcat/tomcat9/webapps/
 CONTAINERREPO=/var/lib/docker/volumes/myvolume/_data/
 DOCKERIMAGES=oinia2/aws-spring-web:0.94
-REPLICAS_NUM=2
-COMPOSE_FILE=/home/ec2-user/app/test/zip/web3.yml
+COMPOSE_FILE=web.yml
+MONITOR_FILE=prometheus.yml
+APP_NAME=myapp
+MONITOR_NAME=pro
 
+echo "> (외장톰캣사용시)Build 파일 복사 "
 
-echo "> Build 파일 복사 "
+sudo cp ${REPOSITORY}/*.war ${WEBREPOSITORY}
 
-sudo cp $REPOSITORY/*.war $WEBREPOSITORY
-
-echo "> 컨테이너 내부로 재배포 "
-sudo cp $REPOSITORY/*.war $CONTAINERREPO
-sudo cp $REPOSITORY/tomcat.sh $CONTAINERREPO
-
-echo "> docker command start"
-#sudo chmod -R 775 /var/lib/docker/volumes
+echo "> 컨테이너 volume 생성 및 volume 으로 배포 "
 sudo docker volume create --name myvolume
-sudo docker stack deploy --compose-file=$COMPOSE_FILE myapp
+sudo chmod -R 775 /var/lib/docker/volumes
+sudo cp ${REPOSITORY}/*.war ${CONTAINERREPO}
 
-echo "> 현재 구동중인 애플리케이션 pid 확인 "
+echo "> docker stack start"
+sudo docker stack deploy --compose-file=${REPOSITORY}/{$COMPOSE_FILE} ${APP_NAME}
+sudo docker stack deploy --compose-file=${REPOSITORY}/${MONITOR_NAME} ${MONITOR_NAME}
 
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.war)
+#echo "> 현재 구동중인 애플리케이션 pid 확인 "
 
-echo ">현재 구동중인 애플리케이션 pid : $CURRENT_PID"
+#CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.war)
 
-if [ -z "$CURRENT_PID" ]; then
-	echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-	echo "> kill -15 $CURRENT_PID"
-	kill -15 $CURRENT_PID
-	sleep 5
-fi
-	echo "> 새 애플리케이션 배포"
+#echo ">현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 
-	JAR_NAME=$(ls -t $WEBREPOSITORY/ | grep war | tail -n 1)
-
-	echo "> JAR NAME : $JAR_NAME"
-	
-	nohup java -jar \
-		-Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-uesr/app/application-real-db.properties \
-		-Dspring.profiles.active=real \
-		$JAR_NAME > $REPOSITORY/nohup.out 2>1 &
+#if [ -z "$CURRENT_PID" ]; then
+#	echo ">현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+#else
+#	echo "> kill -15 $CURRENT_PID"
+#	kill -15 $CURRENT_PID
+#	sleep 5
+#fi
+#	echo "> 새 애플리케이션 배포"
+#
+#	JAR_NAME=$(ls -t $WEBREPOSITORY/ | grep war | tail -n 1)
+#
+#	echo "> JAR NAME : $JAR_NAME"
+#	
+#	nohup java -jar \
+#		-Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-uesr/app/application-real-db.properties \
+#		-Dspring.profiles.active=real \
+#		$JAR_NAME > $REPOSITORY/nohup.out 2>1 &
 
 
---
+++ scripts/execute.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+tomcatRepo=/usr/local/tomcat/tomcat9
+fixfile=${tomcatRepo}/webapps/myapp-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/main.jsp
+
+#매개변수 받기(배열로저장)
+containerIDs=("$@")
+
+#매개 변수 개수
+argsNum=$#
+#받은 매개 변수 출력(테스트)
+echo $@
+echo ${containerIDs[0]}
+echo ${containerIDs[1]}
+
+# 볼륨 생성 
+sudo docker volume create --name myvolume
+
+# 받은 개수 만큼 for문으로 명령어 처리
+for ((i=0;i<argsNum;i++)); do
+
+        # 컨테이너의 웹 스크립트 제목 수정.(이걸로 워커들 판별)
+        sudo docker exec ${containerIDs[$i]} /bin/bash -c ${tomcatRepo}/bin/shutdown.sh
+        sudo docker exec ${containerID} /bin/bash -c "sudo cat ${fixfile} | sudo sed s/<title>mom"'"s shopping(worker2)</title>/<title>mom"'"s shopping(${i})</title>/" > ${fixfile}
+        # 컨테이너의 톰캣 실행하기(systemd 데몬이 없기 때문에 직접 실행해야함)
+        sudo docker exec ${containerIDs[$i]} /bin/bash -c ${tomcatRepo}/bin/startup.sh
+        sudo docker exec ${containerIDs} /bin/bash -c "echo 'namesever 8.8.8.8' >> /etc/resolve.conf"
+        sudo docker exec ${containerIDs} /bin/bash -c "yum -y install net-tools"
+	# /app에 배포된 .war 파일을 웹 레포지토리로 배포.
+	sudo docker exec ${containerIDs} /bin/bash -c "cp /app/*.war /usr/local/tomcat/tomcat9/webapps/myapp/"
+
+done
+
